73a0397d1333242b9a82e51283f8de9f18359bb8
Johnny18823 <jw3621@ic.ac.uk>
2023-05-30 16:51:24 +0100

    .

--- Combine.py
+++ Combine.py
@@ -24,10 +24,26 @@ def update_map(map_image, x, y, piece_of_map):
     map_image[y:y+piece_height, x:x+piece_width] = piece_of_map
 
     return map_image
+def create_maze_image(width, height):
+    # Create a blank black image
+    maze_image = np.zeros((height, width), dtype=np.uint8)
 
-Map = create_map(2410,3670)
-map_piece = create_map_black(300,300)
-Map = update_map(Map, 100, 100, map_piece)
+    # Set the walls as white
+    maze_image[:, :] = 255
+
+    # Draw the maze pattern
+    # Adjust the coordinates and sizes to customize the maze design
+    cv2.rectangle(maze_image, (100, 100), (150, 200), 0, -1)
+    cv2.rectangle(maze_image, (200, 200), (300, 300), 0, -1)
+    cv2.rectangle(maze_image, (400, 400), (500, 500), 0, -1)
+    # Add more maze patterns as needed...
+
+    return maze_image
+
+# Map = create_map(2410,3670)
+# map_piece = create_map_black(300,300)
+# Map = update_map(Map, 100, 100, map_piece)
+Map = create_maze_image(500,800)
 
 cv2.imshow('Image', Map)
 cv2.waitKey(10000)
