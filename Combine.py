1ac955f5515f98930ffc6d4b1c1c6956961d94c7
Johnny18823 <jw3621@ic.ac.uk>
2023-05-31 17:55:16 +0100

    rotate image after transform and add to main map

--- Combine.py
+++ Combine.py
@@ -1,50 +1,56 @@
 import cv2
 import numpy as np
 
-# def AddMap (new_map, rob_x, rob_y, xy_angle, z_angle, Map):
-#     map_x = rob_x + 100
-#     map_y = rob_y + 100
-def create_map(width, height):
+
+def single_color_image(width, height, color):
     # Create a blank black image
     map_image = np.zeros((height, width), dtype=np.uint8)
-    # map_image[map_image == 0] = 255
+    map_image[map_image == 0] = color
     return map_image
 
-def create_map_black(width, height):
-    # Create a blank black image
-    map_image = np.zeros((height, width), dtype=np.uint8)
-    map_image[map_image == 0] = 255
-    return map_image
 
-def update_map(map_image, x, y, piece_of_map):
-    # Get the dimensions of the piece of map
-    piece_height, piece_width = piece_of_map.shape[:2]
-
-    # Update the corresponding region in the map image with the piece of map
-    map_image[y:y+piece_height, x:x+piece_width] = piece_of_map
-
-    return map_image
 def create_maze_image(width, height):
-    # Create a blank black image
     maze_image = np.zeros((height, width), dtype=np.uint8)
-
-    # Set the walls as white
     maze_image[:, :] = 255
-
-    # Draw the maze pattern
-    # Adjust the coordinates and sizes to customize the maze design
     cv2.rectangle(maze_image, (100, 100), (150, 200), 0, -1)
     cv2.rectangle(maze_image, (200, 200), (300, 300), 0, -1)
     cv2.rectangle(maze_image, (400, 400), (500, 500), 0, -1)
-    # Add more maze patterns as needed...
 
     return maze_image
 
-# Map = create_map(2410,3670)
-# map_piece = create_map_black(300,300)
-# Map = update_map(Map, 100, 100, map_piece)
-Map = create_maze_image(500,800)
+def update_map(map_image, x, y, map_piece):
+    # Get the dimensions of the piece of map
+    piece_height, piece_width = map_piece.shape[:2]
+    print (piece_height, piece_width) 
+    # Iterate over the pixels in the map piece
+    for i in range(piece_height):
+        for j in range(piece_width):
+            print (map_piece[i, j])
+            if (map_piece[i, j] == 255 and map_image[y+i, x+j] >= 30):
+                map_image[y+i, x+j] -= 30
 
-cv2.imshow('Image', Map)
-cv2.waitKey(10000)
-cv2.destroyAllWindows()
+    return map_image
+
+
+
+# True_Map = create_maze_image(500,800)
+# Cur_Map = single_color_image(200,200)
+# cv2.imshow('True', True_Map)
+# cv2.imshow('Cur', Cur_Map)
+# cv2.waitKey(5000)
+# cv2.destroyAllWindows()
+
+# True_Map = update_map(True_Map, 100, 100, Cur_Map)
+# # cv2.circle(True_Map, (10, 10), 2, (0, 0, 255), -1)
+# cv2.imshow('True', True_Map)
+# # cv2.imshow('Cur', Cur_Map)
+# cv2.waitKey(10000)
+# cv2.destroyAllWindows()
+
+
+
+# while True:
+    # x,y,map_piece = image_transform()
+    # Cur_Map = update_map(Cur_Map, 0, 0, map_piece)
+    # cv2.imshow('True', True_Map)
+    # cv2.imshow('Cur', Cur_Map)
--- perspectivetest.py
+++ perspectivetest.py
@@ -2,20 +2,41 @@ import cv2
 import numpy as np
 
 # Load image
-img = cv2.imread("img/light_maze1.jpeg", 0)
+# img = cv2.imread("img/light_maze1.png", 0)
 
-# Calculate the target width or height while maintaining the aspect ratio
-target_size = 1080
-height, width = img.shape
+# # Calculate the target width or height while maintaining the aspect ratio
+# target_size = 1080
+# height, width = img.shape
 
-if width > height:
-    new_width = target_size
-    new_height = int(height * (new_width / width))
-else:
-    new_height = target_size
-    new_width = int(width * (new_height / height))
+# if width > height:
+#     new_width = target_size
+#     new_height = int(height * (new_width / width))
+# else:
+#     new_height = target_size
+#     new_width = int(width * (new_height / height))
+def img_transform(img_src):
+    img = cv2.imread(img_src, 0)
 
-def make_threshold(img):
+    # Calculate the target width or height while maintaining the aspect ratio
+    target_size = 1080
+    height, width = img.shape
+
+    if width > height:
+        new_width = target_size
+        new_height = int(height * (new_width / width))
+    else:
+        new_height = target_size
+        new_width = int(width * (new_height / height))
+
+    result1 = make_threshold(img, new_width, new_height)
+    original_points = np.float32([[0, 0], [new_width, 0], [0, new_height], [new_width, new_height]])
+    transformed_points = np.float32([[0, 0], [new_width, 0], [-new_width, 1.5* new_height], [2*new_width, 1.5 * new_height]])
+    matrix = cv2.getPerspectiveTransform(transformed_points, original_points)
+    result2 = cv2.warpPerspective(result1, matrix, (new_width, new_height))
+
+    return result1, result2
+
+def make_threshold(img, new_width, new_height):
     # Resize the image with the calculated dimensions
     resized_img = cv2.resize(img, (new_width, new_height))
 
@@ -32,20 +53,32 @@ def make_threshold(img):
 
     return closed_img
 
-result1 = make_threshold(img)
+# result1 = make_threshold(img)
 
-# Resize the window to match the image dimensions
-cv2.namedWindow('Result', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Result', new_width, new_height)
+# # Resize the window to match the image dimensions
+# cv2.namedWindow('Result', cv2.WINDOW_NORMAL)
+# cv2.resizeWindow('Result', new_width, new_height)
 
-# Perform perspective transform
-original_points = np.float32([[0, 0], [new_width, 0], [0, new_height], [new_width, new_height]])
-transformed_points = np.float32([[0, 0], [new_width, 0], [-new_width, 1.5* new_height], [2*new_width, 1.5 * new_height]])
-matrix = cv2.getPerspectiveTransform(transformed_points, original_points)
-result2 = cv2.warpPerspective(result1, matrix, (new_width, new_height))
+# # Perform perspective transform
+# original_points = np.float32([[0, 0], [new_width, 0], [0, new_height], [new_width, new_height]])
+# transformed_points = np.float32([[0, 0], [new_width, 0], [-new_width, 1.5* new_height], [2*new_width, 1.5 * new_height]])
+# matrix = cv2.getPerspectiveTransform(transformed_points, original_points)
+# result2 = cv2.warpPerspective(result1, matrix, (new_width, new_height))
 
-# Display the image
-cv2.imshow('Result', result1)
-cv2.imshow('Result2', result2)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+if (0):
+    result1, result2 = img_transform("img/light_maze1.png")
+    # Display the image
+    cv2.imshow('Result', result1)
+    cv2.imshow('Result2', result2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
