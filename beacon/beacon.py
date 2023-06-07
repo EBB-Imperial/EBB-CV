4690ef5c10fa2d3e3535303dfa29a5f673b183e3
Johnny18823 <jw3621@ic.ac.uk>
2023-06-07 15:10:15 +0100

    add beacon location

--- beacon/beacon.py
+++ beacon/beacon.py
@@ -29,21 +29,26 @@ def find_pos(alpha, beta, w, h, diff=0.5):
     return list
 
 
-def find_pos_1(alpha, beta, w, h, diff=0.5):
-    beacon1 = (0, h)
-    beacon2 = (w, h)
-    beacon3 = (w, 0)
-    # w = float(w)
-    # h = float(h)
-    
+def find_pos_1(alpha, beta, w, h):
+
+    w = float(w)
+    h = float(h)
+    # alpha = sp.rad(alpha)
+    # beta = sp.rad(beta)
+    alpha = alpha*sp.pi/180
+    beta = beta*sp.pi/180
+    print(alpha, beta)
+
+    a = sp.Symbol('a')
     x = sp.Symbol('x')
 
-    equation = sp.Eq(10 - 2*x, 3*x)
+    equation_1 = sp.Eq(a * sp.tan(beta - x), 10 - a * sp.tan(x))
+    equation_2 = sp.Eq(10 - a * sp.tan(x), 10 - (10-a) * sp.tan(sp.pi - alpha - x))
+ 
+    solution = sp.nsolve((equation_1,equation_2), (a, x), (1, 1))
 
-    solution = sp.solve(equation, x)
-  
-    # return solution[x], solution[y]
-
+    print(solution)
+    return solution
 
 def plot(width, height, list):
     origin_x, origin_y = 0, 0
@@ -55,9 +60,11 @@ def plot(width, height, list):
     ax.add_patch(rectangle)
 
     # Draw the point on the rectangle as a red circle
-    for element in list:
-        [point_x, point_y] = element
-        ax.plot(point_x, point_y, marker='o', markersize=1, color='red')
+    # for element in list:
+    #     point_x,theta = element[0], element[1]
+    point_x,theta = list[0], list[1]
+    point_y = height - point_x * math.tan(theta)
+    ax.plot(point_x, point_y, marker='o', markersize=7, color='red')
 
     # Set the axis limits based on the rectangle dimensions
     ax.set_xlim(origin_x, origin_x + width)
@@ -70,6 +77,8 @@ def plot(width, height, list):
     # Display the plot
     plt.show()
 
-list = find_pos(120, 45, 100, 50, 0.01)
+# list = find_pos(120, 45, 100, 50, 0.01)
+# plot(100, 50, list)
 
-plot(100, 50, list)
+solution = find_pos_1(150, 80, 10, 10)
+plot(10, 10, solution)
